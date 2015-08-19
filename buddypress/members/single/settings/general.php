bb596c23d6a3dfec1f078d4ea2f5ef48969ca50e
Randi Miller <randi@geekguy.co>
2015-08-18 23:12:37 -0400

    Formatting the user's profile.

--- buddypress/members/single/settings/general.php
+++ buddypress/members/single/settings/general.php
@@ -7,13 +7,13 @@ do_action( 'bp_before_member_settings_template' ); ?>
 
 	<?php if ( !is_super_admin() ) : ?>
 
-		<label for="pwd"><?php _e( 'Current Password <span>(required to update email or change current password)</span>', 'buddypress' ); ?></label>
-		<input type="password" name="pwd" id="pwd" size="16" value="" class="settings-input small" <?php bp_form_field_attributes( 'password' ); ?>/> &nbsp;<a href="<?php echo wp_lostpassword_url(); ?>" title="<?php esc_attr_e( 'Password Lost and Found', 'buddypress' ); ?>"><?php _e( 'Lost your password?', 'buddypress' ); ?></a>
+		<label for="pwd"><?php _e( 'Current Password <span>(required to change current password)</span>', 'buddypress' ); ?></label>
+		<input type="password" name="pwd" id="pwd" size="16" value="" class="settings-input small" <?php bp_form_field_attributes( 'password' ); ?>/>
 
 	<?php endif; ?>
 
-	<label for="email"><?php _e( 'Account Email', 'buddypress' ); ?></label>
-	<input type="email" name="email" id="email" value="<?php echo bp_get_displayed_user_email(); ?>" class="settings-input" <?php bp_form_field_attributes( 'email' ); ?>/>
+<!--	<label for="email">--><?php //_e( 'Account Email', 'buddypress' ); ?><!--</label>-->
+	<input type="hidden" name="email" id="email" value="<?php echo bp_get_displayed_user_email(); ?>" class="settings-input" <?php bp_form_field_attributes( 'email' ); ?>/>
 
 	<label for="pass1"><?php _e( 'Change Password <span>(leave blank for no change)</span>', 'buddypress' ); ?></label>
 	<input type="password" name="pass1" id="pass1" size="16" value="" class="settings-input small password-entry" <?php bp_form_field_attributes( 'password' ); ?>/> &nbsp;<?php _e( 'New Password', 'buddypress' ); ?><br />
